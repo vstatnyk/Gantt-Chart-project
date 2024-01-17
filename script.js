1991a88be1bf8785b5da669ccb2f9f3c507246fa
VLAD_STATNYK <vlad.statnyk@gmail.com>
2024-01-16 22:09:33 -0800

    completed instruction manual

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const URL =
-  "https://script.google.com/macros/s/AKfycbxjj3o0eXaiFgDM9zsTAYJFBKuH25F0l5aW8kLJqFeROqEtpCnpkna44otimrKWTtU/exec";
+  "https://script.google.com/macros/s/AKfycbz5TUz7wei-0Wafq7y3QdFQcdI_7Pkvx6HLf4N4FGi1f-qInNzhhZkOQYI2p0mQu5A/exec";
 
-async function getResults() {
-  // send get request to URL
-  const get = await fetch(URL).then((res) => res.json());
-  console.log(get);
+async function getResults(id) {
+  console.log(URL + "?id=" + String(id));
+  const get = await fetch(URL + "?id=" + String(id)).then((res) => res.json());
+  // console.log(get);
   let rows = [];
 
   for (i in get) {
     // console.log(get[i]);
     for (j in get[i]) {
+      // console.log(get[i][j]);
       start = splitDate(get[i][j]["start"]);
-      console.log(start);
+      // console.log(start);
       end = splitDate(get[i][j]["end"]);
-      aniticipatedEnd = splitDate(get[i][j]["aniticipatedEnd"]);
+      // console.log(end);
+      if (end.length == 1) {
+        end = splitDate(get[i][j]["aniticipatedEnd"]);
+      }
+      // console.log(end);
+      // aniticipatedEnd = splitDate(get[i][j]["aniticipatedEnd"]);
       rows.push([
         i + j,
         get[i][j]["name"],
         i,
-        new Date(Number(start[0]), Number(start[2]), Number(start[1])),
-        validEndDate(end, aniticipatedEnd),
+        new Date(Number(start[0]), Number(start[1]), Number(start[2])),
+        new Date(Number(end[0]), Number(end[1]), Number(end[2])),
         null,
         100,
         null,
       ]);
-      // console.log(new Date(Number(end[0]), Number(end[1]), Number(end[2])));
     }
   }
-  // console.log(new Date(Number(end[0]), Number(end[1]), Number(end[2])));
 
   return rows;
 }
@@ -90,4 +94,17 @@ function validEndDate(date1, date2) {
     // console.log("else");
     return new Date(Number(date2[0]), Number(date2[2]), Number(date2[1]));
   }
+}
+
+async function initiateProcess() {
+  // Get the sheet_id value from the input
+
+  const sheetId = document.getElementById("sheet_id").value;
+  console.log(sheetId);
+
+  // Call getResults with the provided sheet_id
+  const data = await getResults(sheetId);
+
+  // Draw the chart with the retrieved data
+  drawChart(data);
 }
